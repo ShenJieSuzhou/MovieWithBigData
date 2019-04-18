e8e2a02884af2b1fb45eccf699bcb9bdace5b505
ShenJieSuzhou <745689122@qq.com>
2019-04-18 17:31:24 +0800

    初始化工程
    初始化工程

---
+++ MLMovies/MLMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLMovies
+//
+//  Created by shenjie on 2019/4/18.
+//  Copyright © 2019年 shenjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
