baf45c2a3dcc8689089849adcbacca9ea49e262e
ShenJieSuzhou <745689122@qq.com>
2019-04-19 17:46:49 +0800

    更改 Main.storyboard 指向的class
    更改 Main.storyboard 指向的class

--- MLMovies/MLMovies/AppDelegate.swift
+++ MLMovies/MLMovies/AppDelegate.swift
@@ -12,10 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var root: HomeViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+//        root = HomeViewController()
+//        self.window?.rootViewController = root
+//        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ MLMovies/MLMovies/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  MLMovies
+//
+//  Created by shenjie on 2019/4/19.
+//  Copyright © 2019年 shenjie. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+}
